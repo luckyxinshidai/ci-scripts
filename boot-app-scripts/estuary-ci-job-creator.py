5bffa97079b24ddddaad0991170de2691c2da6d7
zhaoshijie <zhaoshijiegz@163.com>
2017-04-11 21:33:18 +0800

    reback

--- boot-app-scripts/estuary-ci-job-creator.py
+++ boot-app-scripts/estuary-ci-job-creator.py
@@ -289,7 +289,7 @@ def create_jobs(base_url, kernel, plans, platform_list, targets, priority,
                                         if 'boot' not in plan and 'BOOT' not in plan:
                                             tmp = tmp.replace('{device_type}', 'dummy_ssh'+'_'+device_type)
                                         else:
-                                            tmp = tmp.replace('{device_type}', device_type)
+                                            tmp = tmp.replace('{device_type}', device_type+'_zsj_test')
                                         tmp = tmp.replace('{job_name}',\
                                                 job_json.split("/")[-1].split(".json")[0])
                                         if sasFlag:
